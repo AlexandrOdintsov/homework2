0524d3200ae60da3908e68bef1591919ede4600b
AlexandrOdintsov <alexodin83@mail.ru>
2024-06-29 19:29:45 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+Kolichestvo_vypolnennyh_DZ=12
+print("Количество выполненных ДЗ", Kolichestvo_vypolnennyh_DZ)
+Kolichestvo_zatrachennyh_chasov=1.5
+print("Количество затраченных часов", Kolichestvo_zatrachennyh_chasov)
+Nazvanie_kursa="Pyton"
+print("Название курса", Nazvanie_kursa)
+Vremya_na_odno_Zadanie=1.5/12*60
+print("Время на одно задание,мин.", Vremya_na_odno_Zadanie)
+
